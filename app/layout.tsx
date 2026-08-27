490ba1da2d8a5010c08d047679fc2723f01b972d
JGOJLG <jen@jgohire.com>
2026-08-27 15:20:46 -0400

    Use password-only gate for JLG Core

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import CompletedVisibilityControl from "./CompletedVisibilityControl";
+import PasswordGate from "./PasswordGate";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <PasswordGate />
         <CompletedVisibilityControl />
       </body>
     </html>
